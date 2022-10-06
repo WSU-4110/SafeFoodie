fac6234b539d1adb203100dbcc921c66117c24f0
Savannah Papp <spapp1113@gmail.com>
2022-10-06 16:06:55 -0400

    import firebase packages

--- safefoodie_login_merge/lib/firebase_options.dart
+++ safefoodie_login_merge/lib/firebase_options.dart
@@ -3,7 +3,14 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
+// ...
+
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
 /// Example:
