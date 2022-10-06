749db7548f83f7e858f162654093038c70367bb5
lukejavier <80496198+lukejavier@users.noreply.github.com>
2022-10-06 19:12:58 -0400

    Create sf_loginfunc.dart

---
+++ sf_loginfunc.dart
@@ -0,0 +1,174 @@
+//1
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:safefoodie_loginscreen.dart';
+
+//2
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+void main() {
+  runApp(MyApp());
+}
+
+//3
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Auth Demo',
+      home: MyHomePage(title: 'Firebase Auth Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+//4
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          Builder(builder: (BuildContext context) {
+//5
+            return FlatButton(
+              child: const Text('Sign out'),
+              textColor: Theme.of(context).buttonColor,
+              onPressed: () async {
+                final FirebaseUser user = await _auth.currentUser();
+                if (user == null) {
+//6
+                  Scaffold.of(context).showSnackBar(const SnackBar(
+                    content: Text('No one has signed in.'),
+                  ));
+                  return;
+                }
+                await _auth.signOut();
+                final String uid = user.uid;
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text(uid + ' has successfully signed out.'),
+                ));
+              },
+            );
+          })
+        ],
+      ),
+      body: Builder(builder: (BuildContext context) {
+//7
+        return ListView(
+          scrollDirection: Axis.vertical,
+          padding: const EdgeInsets.all(16),
+          children: <Widget>[
+            _RegisterEmailSection(),
+          ],
+        );
+      }),
+    );
+  }
+}
+
+class _RegisterEmailSection extends StatefulWidget {
+  final String title = 'Registration';
+  @override
+  State<StatefulWidget> createState() => _RegisterEmailSectionState();
+}
+
+class _RegisterEmailSectionState extends State<_RegisterEmailSection> {
+  @override
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _success;
+  String _userEmail;
+  Widget build(BuildContext context) {
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                alignment: Alignment.center,
+                child: RaisedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      _register();
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: Text(_success == null
+                    ? ''
+                    : (_success
+                        ? 'Successfully registered ' + _userEmail
+                        : 'Registration failed')),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+  }
+}
+
+void _register() async {
+  final FirebaseUser user = (await _auth.createUserWithEmailAndPassword(
+    email: _emailController.text,
+    password: _passwordController.text,
+  ))
+      .user;
+  if (user != null) {
+    setState(() {
+      _success = true;
+      _userEmail = user.email;
+    });
+  } else {
+    setState(() {
+      _success = true;
+    });
+  }
+}
+
+@override
+void dispose() {
+  _emailController.dispose();
+  _passwordController.dispose();
+  super.dispose();
+}
