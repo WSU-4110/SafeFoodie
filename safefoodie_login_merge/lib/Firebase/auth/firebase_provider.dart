10b06ade97ed80b1f566c2597ae4ec358621f87a
Aakash Mukhi <akmukhi3@gmail.com>
2022-10-21 17:10:24 -0400

    Added imports

--- safefoodie_login_merge/lib/Firebase/auth/firebase_provider.dart
+++ safefoodie_login_merge/lib/Firebase/auth/firebase_provider.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart'
     show FirebaseAuth, FirebaseAuthException;
 import 'package:firebase_core/firebase_core.dart';
-//import 'package:';
+import 'package:safefoodie_login_merge/Firebase/firebase_options.dart';
 import 'package:safefoodie_login_merge/Firebase/auth/user.dart';
 import 'package:safefoodie_login_merge/Firebase/auth/provider.dart';
 import 'package:safefoodie_login_merge/Firebase/auth/auth_exceptions.dart';
-import 'package:safefoodie_login_merge/Firebase/firebase_options.dart';
 
 class FirebaseAuthProvider implements Provider {
   @override
