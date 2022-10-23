97f768691d8d1428e89355d668bea59a13c7c28e
Aakash Mukhi <akmukhi3@gmail.com>
2022-10-23 19:51:59 -0400

    Added stuff for the bloc still needs debugging

---
+++ safefoodie_login_merge/lib/Firebase/auth/state.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:safefoodie_login_merge/Firebase/auth/user.dart';
+import 'package:equatable/equatable.dart';
+
+@immutable
+abstract class State {
+  final bool loading;
+  final String? loadingText;
+  const State({
+    required this.loading,
+    this.loadingText = 'Please wait',
+  });
+}
+
+//state that something is not initialized
+class StateUninitialized extends State {
+  const StateUninitialized({required bool loading}) : super(loading: loading);
+}
+
+//Registration state
+class StateRegistering extends State {
+  final Exception? exception;
+  const StateRegistering({
+    required this.exception,
+    required bool loading,
+  }) : super(loading: loading);
+}
+
+//Reset Password state
+class StateResetPassword extends State {
+  final Exception? exception;
+  final bool sentEmail;
+  const StateResetPassword({
+    required this.exception,
+    required this.sentEmail,
+    required this.loading,
+  }) : super(loading: loading);
+}
+
+//Logged in state
+class StateLoggedIn extends State {
+  final AuthUser user;
+  const StateLoggedIn({
+    required this.user,
+    required bool loading,
+  }) : super(loading: loading);
+}
+
+//email verification state
+class StateVerification extends State {
+  const StateVerification({required bool loading}) : super(loading: loading);
+}
+
+//Logged out state
+class StateLoggedOut extends State with EquatableMixin {
+  final Exception? exception;
+  const StateLoggedOut({
+    required this.exception,
+    required bool loading,
+    String? loadingText,
+  }) : super(loading: loading, loadingText: loadingText);
+
+  @override
+  List<Object> get props => [exception, loading];
+}
