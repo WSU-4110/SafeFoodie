58d51a65e92b77b50b9f21ad879a09bcf432d1d9
Arbanzo <94333455+Arbanzo@users.noreply.github.com>
2022-10-25 16:41:34 -0400

    Updated homescreen further
    Merged mine and alfreds homescreens further

--- safefoodie_login_merge/lib/homescren.dart
+++ safefoodie_login_merge/lib/homescren.dart
@@ -5,13 +5,11 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
- 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:  'Flutter Demo',
-  theme: ThemeData(
+      title: 'Home',
+      theme: ThemeData(
         primarySwatch: Colors.green,
         scaffoldBackgroundColor:
             Color.fromARGB(166, 72, 168, 75), //color of background
@@ -35,61 +33,131 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
-      'Index 0: view',
+      'My lists',
       style: optionStyle,
     ),
     Text(
-      'Index 1: settings',
+      'Search Lists',
       style: optionStyle,
     ),
     Text(
-      'Index 2: Create',
+      'Filler',
       style: optionStyle,
     ),
     Text(
-      'Index 3: Search',
+      'Settings',
       style: optionStyle,
     ),
   ];
 
   void _onItemTapped(int index) {
-  setState(() {
+    setState(() {
       _selectedIndex = index;
-   });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BottomNavigationBar Sample'),
-      ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Create List
+        },
+        label: const Text('Create New List'),
+        icon: Icon(Icons.add_circle),
+        backgroundColor: Colors.green,
+      ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
+        type: BottomNavigationBarType.fixed, // Fixed
+        backgroundColor: Colors.green, // <-- This works for fixed
+        selectedItemColor: Color.fromARGB(216, 230, 182, 53),
+        unselectedItemColor: Colors.brown,
+        items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.align_horizontal_left),
-            label: 'View',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'settings',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle),
-            label: 'Add',
+            label: 'View Lists',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.search),
-            label: 'search',
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Account',
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Color.fromARGB(216, 230, 182, 53),
         onTap: _onItemTapped,
       ),
     );
   }
 }
+
+/*
+// Nav function for my lits page
+void _ToMyListsScreen(BuildContext context) {
+  Navigator.of(context)
+// .push(MaterialPageRoute(builder: (context) => MyListsPage()));
+}
+
+//nav function for Search Lists page
+void _ToSearchListScreen(BuildContext context) {
+  Navigator.of(context)
+    //  .push(MaterialPageRoute(builder: (context) => SearchListsPage()));
+}*/
+
+/*Nav function for settings Page
+void _ToSettingsScreen(BuildContext context) {
+  Navigator.of(context)
+      .push(MaterialPageRoute(builder: (context) => SettingsPage()));
+}*/
+//
+//======================================================================
+// INITIALIZATION OF PAGE CLASS
+//======================================================================
+
+/*class MyListsScreen extends StatefulWidget {
+  //signup
+  @override
+  _MyListsPageState createState() => _MyListsPageState();
+}*/
+
+// class SearchListScreen extends StatefulWidget {
+  //signup
+  //@override
+ // _SearchListPageState createState() => _SearchListPageState();
+//}
+
+//class SettingsPage extends StatefulWidget {
+  //signup
+ // @override
+ // _SettingsPageState createState() => _SettingsPageState();
+//}
+
+/* class _MyListsPageState extends State<MyListsScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  late bool _sucess;
+  late String _userEmail;
+
+  void _register() async {
+    final User? user = (await _auth.createUserWithEmailAndPassword(
+            email: _emailController.text, password: _passwordController.text))
+        .user;
+
+    if (user != null) {
+      setState(() {
+        _sucess = true;
+        _userEmail = user.email!;
+      });
+    } else {
+      setState(() {
+        _sucess = false;
+      });
+    }
+//  }
+*/
+//}
