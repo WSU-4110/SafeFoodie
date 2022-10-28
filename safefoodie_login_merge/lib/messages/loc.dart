e1f636f330964c118310141b41c5f2cf5b91336e
Savannah Papp <spapp1113@gmail.com>
2022-10-28 02:35:29 -0400

    creating localization file/messages

--- safefoodie_login_merge/lib/messages/loc.dart
+++ safefoodie_login_merge/lib/messages/loc.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart' show BuildContext;
-import 'package:flutter_gen_core/flutter_generator.dart' show AppLocalizations;
+import '' show AppLocalizations;
 
 extension Localization on BuildContext {
   AppLocalizations get loc => AppLocalizations.of(this)!;
