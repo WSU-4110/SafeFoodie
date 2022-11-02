4dd4962b290068d4ef5ed2f6981010c9b6f56254
Jason <jasonruan18@gmail.com>
2022-11-02 00:40:47 -0400

    manually merge some files from Arben
    -Imported the setting, search and list .dart files in
    -added settings_ui into dependencies

--- safefoodie_login_merge/lib/GoogleMap.dart
+++ safefoodie_login_merge/lib/GoogleMap.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'main.dart';
 
+// Map Test
 class MapSample extends StatefulWidget {
   @override
   State<MapSample> createState() => MapSampleState();
--- safefoodie_login_merge/lib/main.dart
+++ safefoodie_login_merge/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 // Must Import .Dart inorder to build the widgets
@@ -10,6 +11,9 @@ import 'Rest_Password_page.dart';
 import 'signup.dart';
 import 'Mainpage.dart';
 import 'GoogleMap.dart';
+import 'lists.dart';
+import 'Search.dart';
+import 'Account.dart';
 
 ///////////////////////////////////////////////////
 
@@ -24,7 +28,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SafeFoodie',
       theme: ThemeData(
         primarySwatch: Colors.green,
         scaffoldBackgroundColor:
@@ -43,6 +47,12 @@ class _MyAppState extends State<MyApp> {
         'ResetPasswordScreen': (context) => ResetPasswordScreen(),
         // When navigating to the "Mainpage" route, build the Mainpage widget.
         'Mainpage': (context) => Mainpage(),
+        // When navigating to the "Settings" route, build the Mainpage widget.
+        'Settings': (context) => Settings(),
+        // When navigating to the "Mainpage" route, build the Mainpage widget.
+        'SearchPage': (context) => SearchPage(),
+        // When navigating to the "Mainpage" route, build the Mainpage widget.
+        'ListsPage': (context) => ListsPage(),
         'Map': (context) => MapSample(),
       },
     );
