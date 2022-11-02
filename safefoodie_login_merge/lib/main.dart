a35245237cbb12aae8847f0570832e640379ecb4
akmukhi <92871846+akmukhi@users.noreply.github.com>
2022-11-02 14:48:24 -0400

    New additions for the main.dart file

--- safefoodie_login_merge/lib/main.dart
+++ safefoodie_login_merge/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:safefoodie_login_merge/views/signup.dart';
+import 'package:safefoodie_login_merge/views/login.dart';
 
 void main() => runApp(MyApp());
 
