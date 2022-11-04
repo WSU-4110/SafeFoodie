736048006d7b546b33d3a513dc762f8b14e7fc4f
Aakash Mukhi <akmukhi3@gmail.com>
2022-11-03 23:53:44 -0400

    tried fixing the android embedding it did not work

--- safefoodie_login_merge/ios/Flutter/flutter_export_environment.sh
+++ safefoodie_login_merge/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Savannah\Documents\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Savannah\Documents\GitHub\SafeFoodie\safefoodie_login_merge"
+export "FLUTTER_ROOT=C:\Users\akmuk\Documents\Projects\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\akmuk\Documents\Projects\SafeFoodie\safefoodie_login_merge"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
