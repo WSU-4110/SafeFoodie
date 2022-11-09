5aa43015218ea52ccb421dc46ff3b8ab3aad2b39
lukejavier <80496198+lukejavier@users.noreply.github.com>
2022-11-09 02:40:56 -0500

    Assignment 4 DESIGN - Intro to Decorator pattern

--- safefoodie_login_merge/lib/modelClass.dart
+++ safefoodie_login_merge/lib/modelClass.dart
@@ -12,6 +12,7 @@ String foodrecallToJson(Foodrecall data) => json.encode(data.toJson());
 
 class Foodrecall {
   Foodrecall({
+      //these two are the only things needed
     required this.meta,
     required this.results,
   });
@@ -22,6 +23,7 @@ class Foodrecall {
   factory Foodrecall.fromJson(Map<String, dynamic> json) => Foodrecall(
         meta: Meta.fromJson(json["meta"]),
         results:
+            //parses through and puts the strings into a list to sort through!
             List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
       );
 
@@ -33,6 +35,7 @@ class Foodrecall {
 
 class Meta {
   Meta({
+      //no need to add to this because we don't even call it
     required this.disclaimer,
     required this.terms,
     required this.license,
@@ -66,6 +69,7 @@ class Meta {
 
 class Results {
   Results({
+      //these are the required parameters!
     required this.skip,
     required this.limit,
     required this.total,
@@ -90,113 +94,31 @@ class Results {
 
 class Result {
   Result({
-    required this.country,
-    required this.city,
-    required this.address1,
+      //deleted the unrequired vars because we are only using the API to call reason_for_recall but
+      //this section allows me to add any other info from the FDA's API if we want to in the future!
+      //this is the only thing we are calling at this moment for food recall notifications
+      //but gives us the flexibility to add other information if we want
     required this.reasonForRecall,
-    required this.address2,
-    required this.productQuantity,
-    required this.codeInfo,
-    required this.centerClassificationDate,
-    required this.distributionPattern,
-    required this.state,
-    required this.productDescription,
-    required this.reportDate,
-    required this.classification,
     required this.openfda,
-    required this.recallingFirm,
-    required this.recallNumber,
-    required this.initialFirmNotification,
-    required this.productType,
-    required this.eventId,
-    required this.terminationDate,
-    required this.moreCodeInfo,
-    required this.recallInitiationDate,
-    required this.postalCode,
-    required this.voluntaryMandated,
-    required this.status,
   });
-
-  String country;
-  String city;
-  String address1;
+  
+  //this section allows me to add any other info from the FDA's API if we want to in the future!
   String reasonForRecall;
-  String address2;
-  String productQuantity;
-  String codeInfo;
-  String centerClassificationDate;
-  String distributionPattern;
-  String state;
-  String productDescription;
-  String reportDate;
-  String classification;
   Openfda openfda;
-  String recallingFirm;
-  String recallNumber;
-  String initialFirmNotification;
-  String productType;
-  String eventId;
-  String terminationDate;
-  String moreCodeInfo;
-  String recallInitiationDate;
-  String postalCode;
-  String voluntaryMandated;
-  String status;
 
   factory Result.fromJson(Map<String, dynamic> json) => Result(
-        country: json["country"],
-        city: json["city"],
-        address1: json["address_1"],
+        //this section allows me to add any other info from the FDA's API if we want to in the future!
+        //it is dynamic so it allows us to create and manipulate data for our purposes
+        //this converts the json to string!
         reasonForRecall: json["reason_for_recall"] as String,
-        address2: json["address_2"],
-        productQuantity: json["product_quantity"],
-        codeInfo: json["code_info"],
-        centerClassificationDate: json["center_classification_date"],
-        distributionPattern: json["distribution_pattern"],
-        state: json["state"],
-        productDescription: json["product_description"],
-        reportDate: json["report_date"],
-        classification: json["classification"],
         openfda: Openfda.fromJson(json["openfda"]),
-        recallingFirm: json["recalling_firm"],
-        recallNumber: json["recall_number"],
-        initialFirmNotification: json["initial_firm_notification"],
-        productType: json["product_type"],
-        eventId: json["event_id"],
-        terminationDate: json["termination_date"] ?? null,
-        moreCodeInfo: json["more_code_info"] ?? null,
-        recallInitiationDate: json["recall_initiation_date"],
-        postalCode: json["postal_code"],
-        voluntaryMandated: json["voluntary_mandated"],
-        status: json["status"],
       );
 
   Map<String, dynamic> toJson() => {
-        "country": country,
-        "city": city,
-        "address_1": address1,
+        //this section allows me to add any other info from the FDA's API if we want to in the future!
+        //it is dynamic so it allows us to create and manipulate data for our purposes
         "reason_for_recall": reasonForRecall,
-        "address_2": address2,
-        "product_quantity": productQuantity,
-        "code_info": codeInfo,
-        "center_classification_date": centerClassificationDate,
-        "distribution_pattern": distributionPattern,
-        "state": state,
-        "product_description": productDescription,
-        "report_date": reportDate,
-        "classification": classification,
         "openfda": openfda.toJson(),
-        "recalling_firm": recallingFirm,
-        "recall_number": recallNumber,
-        "initial_firm_notification": initialFirmNotification,
-        "product_type": productType,
-        "event_id": eventId,
-        "termination_date": terminationDate ?? null,
-        "more_code_info": moreCodeInfo ?? null,
-        "recall_initiation_date": recallInitiationDate,
-        "postal_code": postalCode,
-        "voluntary_mandated": voluntaryMandated,
-        "status": status,
       };
 }
 
