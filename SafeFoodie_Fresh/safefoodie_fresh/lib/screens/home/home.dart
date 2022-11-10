ad4d3eed8eedc30f0816d792646ad2c2a426fd81
Jason <jasonruan18@gmail.com>
2022-11-09 23:01:56 -0500

    Rework the page

---
+++ SafeFoodie_Fresh/safefoodie_fresh/lib/screens/home/home.dart
@@ -0,0 +1,42 @@
+import 'package:safefoodie_fresh/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _Home();
+  }
+}
+
+class _Home extends State<Home> {
+  final AuthService _auth = new AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    final SignOut = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Theme.of(context).primaryColor,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () async {
+          await _auth.signOut();
+        },
+        child: Text(
+          "Log out",
+          style: TextStyle(color: Theme.of(context).primaryColorLight),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to SafeFoodie'),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Center(child: SignOut),
+    );
+  }
+}
