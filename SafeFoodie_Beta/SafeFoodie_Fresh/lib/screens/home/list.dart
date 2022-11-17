fccc852eac90b520576ec6e9690c53e862fc0b07
akmukhi <92871846+akmukhi@users.noreply.github.com>
2022-11-17 16:04:42 -0500

    Added check box

--- SafeFoodie_Beta/SafeFoodie_Fresh/lib/screens/home/list.dart
+++ SafeFoodie_Beta/SafeFoodie_Fresh/lib/screens/home/list.dart
@@ -6,9 +6,8 @@ void main() {
       title: 'Gorcery Lists',
       theme: ThemeData(
         primarySwatch: Colors.green,
-        scaffoldBackgroundColor:
-            Color.fromARGB(166, 72, 168, 75),
-             primaryColor: Color.fromARGB(166, 72, 168, 75), //color of background
+        scaffoldBackgroundColor: Color.fromARGB(166, 72, 168, 75),
+        primaryColor: Color.fromARGB(166, 72, 168, 75), //color of background
         textTheme: Typography().black, //sets default text to black
       ),
       home: ListPage(),
@@ -27,59 +26,74 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPage extends State<ListPage> {
-final List<String> food = <String>[];
-final List<DateUtils> date = <DateUtils>[];
+  final List<String> food = <String>[];
+  final List<DateUtils> date = <DateUtils>[];
   TextEditingController nameController = TextEditingController();
+  bool isChecked = false;
 
-  void addItemToList(){
+  void addItemToList() {
     setState(() {
-      food.insert(0,nameController.text);
+      food.insert(0, nameController.text);
     });
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Grocery List'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: TextField(
-              controller: nameController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Item',
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Grocery List'),
+          ),
+          body: Column(children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Item',
+                ),
               ),
             ),
-          ),
-          RaisedButton(
-            child: Text('Add'),
-            onPressed: () {
-              addItemToList();
-            },
-          ),
-          Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: food.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 50,
-                  margin: EdgeInsets.all(2),
-                  child: Center(
-                    child: Text('${food[index]} (${date[index]})',
-                      style: TextStyle(fontSize: 18),
-                    )
-                  ),
-                );
-              }
-            )
-          )
-        ]
-      )
-    );
+            RaisedButton(
+              child: Text('Add'),
+              onPressed: () {
+                addItemToList();
+              },
+            ),
+            Expanded(
+                child: ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: food.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        height: 50,
+                        margin: EdgeInsets.all(2),
+                        child: Center(
+                            child: Text(
+                          '${food[index]} (${date[index]})',
+                          style: TextStyle(fontSize: 18),
+                        )),
+                      );
+                    }))
+          ]));
+      Color getColor(Set<MaterialState> states) {
+        const Set<MaterialState> interactiveStates = <MaterialState>{
+          MaterialState.pressed,
+          MaterialState.hovered,
+          MaterialState.focused,
+        };
+        if (states.any(interactiveStates.contains)) {
+          return Colors.blue;
+        }
+        return Colors.red;
+      }
+      return Checkbox(checkColor: Colors.white,
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      value: isChecked,
+      onChanged: (bool? value){
+        setState((){
+          isChecked = value!;
+        });
+      },
+    };
   }
-}
 }
