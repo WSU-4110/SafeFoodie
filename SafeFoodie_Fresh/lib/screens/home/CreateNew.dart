61b27735345239df0b78b6ffcab99a35b68949cd
Arbanzo <arbeng99@gmail.com>
2022-12-06 18:24:31 -0500

    Removed problems in create.dart
    solved 38 more problems

--- SafeFoodie_Fresh/lib/screens/home/CreateNew.dart
+++ SafeFoodie_Fresh/lib/screens/home/CreateNew.dart
@@ -16,13 +16,13 @@ class _CreateNew extends State<CreateNew> {
   Widget build(BuildContext ctxt) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Create Page"),
+          title: const Text("Create Page"),
         ),
 body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 2.0),
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 2.0),
         child: GridView.count(
           crossAxisCount: 2,
-          padding: EdgeInsets.all(3.0),
+          padding: const EdgeInsets.all(3.0),
           children: <Widget>[
             makeDashboardItem1("Add to existing list", Icons.low_priority_sharp),
             makeDashboardItem2("Create new list", Icons.library_add_sharp),
@@ -34,7 +34,8 @@ body: Container(
         onPressed: () {
           Navigator.pushNamed(context, 'CreateNew');
         },
-        child: Icon(Icons.add),
+        // ignore: sort_child_properties_last
+        child: const Icon(Icons.add),
         backgroundColor: Colors.green, //sets button color
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation
@@ -42,7 +43,7 @@ body: Container(
       //Bottom Navbar
       bottomNavigationBar: BottomAppBar(
         shape:
-            CircularNotchedRectangle(), //navbar reactiveness to center button
+            const CircularNotchedRectangle(), //navbar reactiveness to center button
         notchMargin: 5, //number of elements on bar
         child: Row(
           //children inside bottom appbar
@@ -51,28 +52,28 @@ body: Container(
           children: <Widget>[
             //View lists button
             IconButton(
-              icon: Icon(Icons.align_horizontal_left, color: Colors.green),
+              icon: const Icon(Icons.align_horizontal_left, color: Colors.green),
               onPressed: () {
                 Navigator.pushNamed(context, 'PageList');
               },
             ),
             //Search item button
             IconButton(
-              icon: Icon(Icons.search, color: Colors.green),
+              icon: const Icon(Icons.search, color: Colors.green),
               onPressed: () {
                 Navigator.pushNamed(context, 'Searchpage');
               },
             ),
             //Route back to home page
             IconButton(
-              icon: Icon(Icons.location_pin, color: Colors.green),
+              icon: const Icon(Icons.location_pin, color: Colors.green),
               onPressed: () {
                 Navigator.pushNamed(context, 'MapSample');
               },
             ),
             //Account page button
             IconButton(
-              icon: Icon(Icons.account_circle_outlined, color: Colors.green),
+              icon: const Icon(Icons.account_circle_outlined, color: Colors.green),
               onPressed: () {
                 //Settings navigator
                 Navigator.pushNamed(context, 'Account');
@@ -87,10 +88,10 @@ body: Container(
     Card makeDashboardItem1(String title, IconData icon) {
     return Card(
         elevation: 1.0,
-        margin: new EdgeInsets.all(8.0),
+        margin: const EdgeInsets.all(8.0),
         child: Container(
-          decoration: BoxDecoration(color: Colors.green),
-          child: new InkWell(
+          decoration: const BoxDecoration(color: Colors.green),
+          child: InkWell(
             onTap: () {
               Navigator.pushNamed(context, 'AddtoList');
             },
@@ -99,18 +100,18 @@ body: Container(
               mainAxisSize: MainAxisSize.min,
               verticalDirection: VerticalDirection.down,
               children: <Widget>[
-                SizedBox(height: 50.0),
+                const SizedBox(height: 50.0),
                 Center(
                     child: Icon(
                   icon,
                   size: 40.0,
-                  color: Color.fromARGB(216, 230, 182, 53),
+                  color: const Color.fromARGB(216, 230, 182, 53),
                 )),
-                SizedBox(height: 20.0),
-                new Center(
-                  child: new Text(title,
+                const SizedBox(height: 20.0),
+                Center(
+                  child: Text(title,
                       style:
-                          new TextStyle(fontSize: 18.0, color: Colors.white)),
+                          const TextStyle(fontSize: 18.0, color: Colors.white)),
                 )
               ],
             ),
@@ -121,10 +122,10 @@ body: Container(
       Card makeDashboardItem2(String title, IconData icon) {
     return Card(
         elevation: 1.0,
-        margin: new EdgeInsets.all(8.0),
+        margin: const EdgeInsets.all(8.0),
         child: Container(
-          decoration: BoxDecoration(color: Colors.green),
-          child: new InkWell(
+          decoration: const BoxDecoration(color: Colors.green),
+          child: InkWell(
             onTap: () {
                 Navigator.pushNamed(context, 'NewList');
             },
@@ -133,18 +134,18 @@ body: Container(
               mainAxisSize: MainAxisSize.min,
               verticalDirection: VerticalDirection.down,
               children: <Widget>[
-                SizedBox(height: 50.0),
+                const SizedBox(height: 50.0),
                 Center(
                     child: Icon(
                   icon,
                   size: 40.0,
-                  color: Color.fromARGB(216, 230, 182, 53),
+                  color: const Color.fromARGB(216, 230, 182, 53),
                 )),
-                SizedBox(height: 20.0),
-                new Center(
-                  child: new Text(title,
+                const SizedBox(height: 20.0),
+                Center(
+                  child: Text(title,
                       style:
-                          new TextStyle(fontSize: 18.0, color: Colors.white)),
+                          const TextStyle(fontSize: 18.0, color: Colors.white)),
                 )
               ],
             ),
