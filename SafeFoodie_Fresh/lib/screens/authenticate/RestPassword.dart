c12d23c25be73175534e278c22b393d22d9f2dec
Jason <jasonruan18@gmail.com>
2022-12-08 02:47:11 -0500

    tweaking some UI

--- SafeFoodie_Fresh/lib/screens/authenticate/RestPassword.dart
+++ SafeFoodie_Fresh/lib/screens/authenticate/RestPassword.dart
@@ -20,6 +20,7 @@ class _RestPasswordState extends State<RestPassword> {
       resizeToAvoidBottomInset: true,
       appBar: AppBar(
         elevation: 0,
+        title: const Text('Reset Password'),
         backgroundColor: Color.fromARGB(166, 72, 168, 75),
       ),
       body: Column(
@@ -27,27 +28,13 @@ class _RestPasswordState extends State<RestPassword> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Padding(
-              padding: EdgeInsets.fromLTRB(20, 20, 20, 8),
-              child: Text(
-                'Reset Password',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 25),
-              ),
+              padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
             ),
             const SizedBox(
               height: 30,
             ),
             const Padding(
               padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                'Email',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 13),
-              ),
             ),
             const SizedBox(
               height: 10,
@@ -62,14 +49,16 @@ class _RestPasswordState extends State<RestPassword> {
                 keyboardType: TextInputType.emailAddress,
                 cursorColor: Colors.white,
                 decoration: const InputDecoration(
-                    border: InputBorder.none,
+                    border: OutlineInputBorder(),
+                    labelStyle: TextStyle(color: Colors.white),
+                    labelText: 'Email',
                     fillColor: Color.fromARGB(166, 72, 168, 75),
                     filled: true,
                     focusedBorder: OutlineInputBorder(
                       borderSide: BorderSide(
-                          color: Color.fromARGB(207, 247, 247, 247),
-                          width: 2.0),
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          color: Color.fromARGB(134, 218, 216, 216),
+                          width: 2.5),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
                     )),
                 onChanged: (value) {
                   setState(() {
@@ -88,7 +77,7 @@ class _RestPasswordState extends State<RestPassword> {
                           .sendPasswordResetEmail(email: email)
                           .then((value) => Navigator.of(context).pop());
                     },
-                    child: const Text('Send Request')))
+                    child: const Text('Reset Password')))
           ]),
     );
   }
