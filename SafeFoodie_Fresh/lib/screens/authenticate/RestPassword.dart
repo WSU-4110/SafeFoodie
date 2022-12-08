3861b67a181ae4b3585c1debf71fbbf1ac468feb
Arbanzo <arbeng99@gmail.com>
2022-12-07 19:34:43 -0500

    edited using jasons suggestions in code review
    Co-Authored-By: AlfieCakez <112589305+AlfieCakez@users.noreply.github.com>

--- SafeFoodie_Fresh/lib/screens/authenticate/RestPassword.dart
+++ SafeFoodie_Fresh/lib/screens/authenticate/RestPassword.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: file_names
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -19,81 +20,73 @@ class _RestPasswordState extends State<RestPassword> {
       appBar: AppBar(
         elevation: 0,
       ),
-      // Removed Unnecessary Container
-     body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(20, 20, 20, 8),
-                child: Text(
-                  'Reset Password',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 25),
-                ),
-              ),
-              // added const
-             const SizedBox(
-                height: 30,
-              ),
-              // addedconst 
-             const Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  'Email',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 13),
-                ),
-              ),
-              // added const 
-             const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: TextField(
-                  style: const TextStyle(
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 8),
+              child: Text(
+                'Reset Password',
+                style: TextStyle(
                     color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  cursorColor: Colors.white,
-                  //added const 
-                  decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      fillColor: Color.fromARGB(178, 54, 134, 17),
-                      filled: true,
-                      focusedBorder: const OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Color.fromARGB(216, 230, 182, 53), width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      )),
-                  onChanged: (value) {
-                    setState(() {
-                      email = value;
-                    });
-                  },
+                    fontWeight: FontWeight.w600,
+                    fontSize: 25),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                'Email',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 13),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
                 ),
+                keyboardType: TextInputType.emailAddress,
+                cursorColor: Colors.white,
+                decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    fillColor: Color.fromARGB(178, 54, 134, 17),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(216, 230, 182, 53), width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    )),
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
               ),
-              // added const
-              const SizedBox(
-                height: 10,
-              ),
-              Center(
-                  child: ElevatedButton(
-                      onPressed: () async {
-                        await FirebaseAuth.instance
-                            .sendPasswordResetEmail(email: email)
-                            .then((value) => Navigator.of(context).pop());
-                      },
-                      //added const 
-                      child: const Text('Send Request')))
-            ]),
-      ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Center(
+                child: ElevatedButton(
+                    onPressed: () async {
+                      await FirebaseAuth.instance
+                          .sendPasswordResetEmail(email: email)
+                          .then((value) => Navigator.of(context).pop());
+                    },
+                    child: const Text('Send Request')))
+          ]),
     );
   }
 }
--- SafeFoodie_Fresh/lib/screens/authenticate/login.dart
+++ SafeFoodie_Fresh/lib/screens/authenticate/login.dart
@@ -1,7 +1,6 @@
 import 'package:safefoodie_fresh/models/loginuser.dart';
 import 'package:safefoodie_fresh/services/auth.dart';
 import 'package:flutter/material.dart';
-import 'package:safefoodie_fresh/screens/authenticate/RestPassword.dart';
 
 class Login extends StatefulWidget {
   final Function? toggleView;
