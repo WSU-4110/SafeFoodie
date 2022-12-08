2f31459b00a5f60d7eef9fc4489d75317741f85a
Sav <spapp1113@gmail.com>
2022-12-08 16:32:15 -0500

    stuff changed

---
+++ SafeFoodie_Fresh/.dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -0,0 +1,66 @@
+//
+// Generated file. Do not edit.
+// This file is generated from template in file `flutter_tools/lib/src/flutter_plugins.dart`.
+//
+
+// @dart = 2.18
+
+import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:geolocator_android/geolocator_android.dart';
+import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
+import 'package:geolocator_apple/geolocator_apple.dart';
+import 'package:google_maps_flutter_ios/google_maps_flutter_ios.dart';
+
+@pragma('vm:entry-point')
+class _PluginRegistrant {
+
+  @pragma('vm:entry-point')
+  static void register() {
+    if (Platform.isAndroid) {
+      try {
+        GeolocatorAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`geolocator_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+      try {
+        GoogleMapsFlutterAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`google_maps_flutter_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    } else if (Platform.isIOS) {
+      try {
+        GeolocatorApple.registerWith();
+      } catch (err) {
+        print(
+          '`geolocator_apple` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+      try {
+        GoogleMapsFlutterIOS.registerWith();
+      } catch (err) {
+        print(
+          '`google_maps_flutter_ios` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    } else if (Platform.isLinux) {
+    } else if (Platform.isMacOS) {
+    } else if (Platform.isWindows) {
+    }
+  }
+}
