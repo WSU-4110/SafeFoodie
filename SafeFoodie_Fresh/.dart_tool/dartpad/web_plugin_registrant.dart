cd84167e90c485a4ea40cf2c55373dd790f26d94
Sav <spapp1113@gmail.com>
2022-12-08 16:41:11 -0500

    values changes

--- SafeFoodie_Fresh/.dart_tool/dartpad/web_plugin_registrant.dart
+++ SafeFoodie_Fresh/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,7 +6,6 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
-import 'package:awesome_notifications/awesome_notifications_web.dart';
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
@@ -17,7 +16,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
-  AwesomeNotificationsWeb.registerWith(registrar);
   FirebaseFirestoreWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
--- SafeFoodie_Fresh/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ SafeFoodie_Fresh/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -16,11 +16,6 @@ public final class GeneratedPluginRegistrant {
   private static final String TAG = "GeneratedPluginRegistrant";
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
     try {
-      flutterEngine.getPlugins().add(new me.carda.awesome_notifications.AwesomeNotificationsPlugin());
-    } catch(Exception e) {
-      Log.e(TAG, "Error registering plugin awesome_notifications, me.carda.awesome_notifications.AwesomeNotificationsPlugin", e);
-    }
-    try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.firestore.FlutterFirebaseFirestorePlugin());
     } catch(Exception e) {
       Log.e(TAG, "Error registering plugin cloud_firestore, io.flutter.plugins.firebase.firestore.FlutterFirebaseFirestorePlugin", e);
