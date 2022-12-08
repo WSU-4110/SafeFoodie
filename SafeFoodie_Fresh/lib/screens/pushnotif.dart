08a3dc9608c78866a49b0a343c271e5cd5711949
Arbanzo <94333455+Arbanzo@users.noreply.github.com>
2022-12-08 18:45:56 -0500

    added navbar to page

--- SafeFoodie_Fresh/lib/screens/pushnotif.dart
+++ SafeFoodie_Fresh/lib/screens/pushnotif.dart
@@ -469,6 +469,61 @@ class NotificationPage extends StatelessWidget {
           ],
         ),
       ),
+      //Center action button
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, 'CreateNew');
+        },
+        // ignore: sort_child_properties_last
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.green, //sets button color
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation
+          .centerDocked, //inidcates pronounced button position
+      //Bottom Navbar
+      bottomNavigationBar: BottomAppBar(
+        shape:
+            const CircularNotchedRectangle(), //navbar reactiveness to center button
+        notchMargin: 5, //number of elements on bar
+        child: Row(
+          //children inside bottom appbar
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            //View lists button
+            IconButton(
+              icon:
+                  const Icon(Icons.align_horizontal_left, color: Colors.green),
+              onPressed: () {
+                Navigator.pushNamed(context, 'PageList');
+              },
+            ),
+            //Search item button
+            IconButton(
+              icon: const Icon(Icons.search, color: Colors.green),
+              onPressed: () {
+                Navigator.pushNamed(context, 'Searchpage');
+              },
+            ),
+            //Route back to home page
+            IconButton(
+              icon: const Icon(Icons.location_pin, color: Colors.green),
+              onPressed: () {
+                Navigator.pushNamed(context, 'MapSample');
+              },
+            ),
+            //Account page button
+            IconButton(
+              icon: const Icon(Icons.account_circle_outlined,
+                  color: Colors.green),
+              onPressed: () {
+                //Settings navigator
+                Navigator.pushNamed(context, 'Account');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
